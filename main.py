d436409554a81341b5ff1777581c85d6c5530e9e
Helindu <111685731+Helindu666@users.noreply.github.com>
2025-10-01 14:36:36 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID , PASSWORD
+
+url="https://raw.githubusercontent.com/Helindu666/pico_ota/"
+
+ota_updater = OTAUpdater(SSID,PASSWORD,url,"main.py")
+
+ota_updater.download_and_install_update_if_available()
